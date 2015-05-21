2c2d88357da9073db6652e838e8dbabd1957f5d3
Enddy Dumbrique <enddynayn@hotmail.com>
2015-05-21 12:29:21 -0700

    Fix require to require_relative.

--- init.rb
+++ init.rb
@@ -1,11 +1,13 @@
 ### POKER ####
+#
 # Launch this Ruby file from the command line
 ###
 
 require 'pry'
 APP_ROOT = File.dirname(__FILE__)
 $:.unshift( File.join(APP_ROOT, 'lib') )
-require_relative 'deal'
+
+require 'deal'
 
 Deal.cards
 
